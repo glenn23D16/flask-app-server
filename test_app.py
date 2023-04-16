becec3fa26f086b67741fcb4573d98cd94f4b339
glenn23D16 <glennpaul-s@live.nl>
2023-04-16 12:59:10 +0200

    Docstrings and hashtag comments added to app.py

--- test_app.py
+++ test_app.py
@@ -4,12 +4,27 @@ from app import app
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    """Create a test client for the application.
+
+    This fixture sets up the test client with the app's configuration set to
+    TESTING mode.
+    It then yields the client for use in test functions.
+    """
+    app.config['TESTING'] = True  # Set TESTING mode for the app configuration
     with app.test_client() as client:
-        yield client
+        yield client  # Yield the test client for use in test functions
 
 
 def test_main_page(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b'Hello, World!' in response.data
+    """Test the main page of the application.
+
+    This function tests the main page by sending a GET request to the root
+    endpoint.
+    It asserts that the response status code is 200 and the "Hello, World!"
+    string is present in the response data.
+    """
+    response = client.get('/')  # Send a GET request to the root endpoint
+    assert response.status_code == 200  # Assert the response status code is
+    # 200 (OK)
+    assert b'Hello, World!' in response.data  # Assert "Hello, World!" is
+    # present in the response data
